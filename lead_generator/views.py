e3729f1b36d6ae76d28a59a78957725f976c53af
Arsen Makovei <makoveyarsen@gmail.com>
2023-06-28 01:15:31 +0300

    Created form, view and template for lead generator

--- lead_generator/views.py
+++ lead_generator/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views import generic, View
 
-from lead_generator.forms import UserCreateForm
+from lead_generator.forms import UserCreateForm, LeadGeneratorForm
 from lead_generator.models import User
 
 
@@ -11,3 +12,13 @@ class UserCreateView(generic.CreateView):
     form_class = UserCreateForm
     template_name = "registration/signup.html"
     success_url = reverse_lazy("login")
+
+
+class LeadGeneratorView(LoginRequiredMixin, View):
+    def get(self, request):
+        form = LeadGeneratorForm()
+        return render(request, "lead_generator/lead_generator.html", {"form": form})
+
+    def post(self, request):
+        form = LeadGeneratorForm(request.POST)
+        return render(request, "lead_generator/lead_generator.html", {"form": form})
